ab84526396ea21c8cc290b71662bb2c7642517f3
szz <lszzup@gmail.com>
2025-02-07 21:45:41 +0800

    a

--- init.lua
+++ init.lua
@@ -14,6 +14,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 
 vim.opt.rtp:prepend(lazypath)
+vim.opt.laststatus = 3
 
 require("options")
 require("keymap")
---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,86 @@
+return {
+	{
+		"github/copilot.vim",
+		config = function()
+			vim.g.copilot_enabled = fale
+			vim.g.copilot_no_tab_map = true
+			vim.g.copilot_assume_mapped = true
+			--vim.g.copilot_proxy = "127.0.0.1:1087"
+			vim.api.nvim_set_keymap("i", "<C-o>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+			vim.api.nvim_set_keymap("i", "<C-u>", 'copilot#Previous()', { silent = true, expr = true })
+			vim.api.nvim_set_keymap("i", "<C-e>", 'copilot#Next()', { silent = true, expr = true })
+			--vim.g.co
+			--vim.g.copilot_no_tab_map = true
+			--vim.api.nvim_set_keymap('n', '<leader>go', ':Copilot<CR>', { silent = true })
+			--vim.api.nvim_set_keymap('n', '<leader>ge', ':Copilot enable<CR>', { silent = true })
+			--vim.api.nvim_set_keymap('n', '<leader>gd', ':Copilot disable<CR>', { silent = true })
+			--vim.api.nvim_set_keymap('i', '<c-p>', '<Plug>(copilot-suggest)', {})
+			---- vim.api.nvim_set_keymap('i', '<c-n>', '<Plug>(copilot-next)', { silent = true })
+			---- vim.api.nvim_set_keymap('i', '<c-l>', '<Plug>(copilot-previous)', { silent = true })
+			--vim.cmd('imap <silent><script><expr> <C-C> copilot#Accept("")')
+			--vim.cmd([[
+			--let g:copilot_filetypes = {
+			--\ 'TelescopePrompt': v:false,
+			--\ }
+			--]])
+		end
+	},
+	{
+		"yetone/avante.nvim",
+		event = "VeryLazy",
+		lazy = false,
+		version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
+		opts = {
+			provider = "qianwen",
+			vendors = {
+				qianwen = {
+					__inherited_from = "openai",
+					api_key_name = "DASHSCOPE_API_KEY",
+					endpoint = "https://dashscope.aliyuncs.com/compatible-mode/v1",
+					model = "qwen2.5-coder-32b-instruct",
+					disable_tools = true, -- disable tools!
+				},
+			},
+		},
+		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+		build = "make",
+		-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+		dependencies = {
+			"stevearc/dressing.nvim",
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			--- The below dependencies are optional,
+			"echasnovski/mini.pick",      -- for file_selector provider mini.pick
+			"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+			"hrsh7th/nvim-cmp",           -- autocompletion for avante commands and mentions
+			"ibhagwan/fzf-lua",           -- for file_selector provider fzf
+			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+			"zbirenbaum/copilot.lua",     -- for providers='copilot'
+			{
+				-- support for image pasting
+				"HakonHarnes/img-clip.nvim",
+				event = "VeryLazy",
+				opts = {
+					-- recommended settings
+					default = {
+						embed_image_as_base64 = false,
+						prompt_for_file_name = false,
+						drag_and_drop = {
+							insert_mode = true,
+						},
+						-- required for Windows users
+						use_absolute_path = true,
+					},
+				},
+			},
+			{
+				-- Make sure to set this up properly if you have lazy=true
+				'MeanderingProgrammer/render-markdown.nvim',
+				opts = {
+					file_types = { "markdown", "Avante" },
+				},
+				ft = { "markdown", "Avante" },
+			},
+		},
+	}
+}
